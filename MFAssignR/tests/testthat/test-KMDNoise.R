40b7deb12c0437fc6545be36be1a7bab46ba409f
Zargham Ahmad <zargham.ahmad12@gmail.com>
2023-12-12 14:45:05 +0100

    added ci

--- MFAssignR/tests/testthat/test-KMDNoise.R
+++ MFAssignR/tests/testthat/test-KMDNoise.R
@@ -1,5 +1,8 @@
 test_that("KMDNoise works", {
-  load("../../data/Raw_Neg_ML.rda")
+  load(system.file("data", "Raw_Neg_ML.rda",
+    package = "MFAssignR",
+    mustWork = TRUE
+  ))
   names(Raw_Neg_ML) <- c("mass", "intensity")
   actual <- KMDNoise(Raw_Neg_ML)
   expect_equal(actual$Noise, 346.0706, tolerance = 1e-4)
