6dd58438f158a0348a05826715664d12f054e5eb
hechth <helge.hecht@recetox.muni.cz>
2023-12-12 14:29:47 +0100

    removed duplicated SNplot function and updated NAMESPACE

--- MFAssignR/tests/testthat/test-KMDNoise.R
+++ MFAssignR/tests/testthat/test-KMDNoise.R
@@ -4,3 +4,11 @@ test_that("KMDNoise works", {
   actual <- KMDNoise(Raw_Neg_ML)
   expect_equal(actual$Noise, 346.0706, tolerance = 1e-4)
 })
+
+
+test_that("SNplot works", {
+  load("../../data/Raw_Neg_ML.rda")
+  head(Raw_Neg_ML)
+  Raw_Neg_ML <- dplyr::relocate(Raw_Neg_ML, intensity) |> dplyr::rename(mass = m.z)
+  SNplot(Raw_Neg_ML, 100, 300)
+})
