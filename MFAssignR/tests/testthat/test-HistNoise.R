7d459b25f56b87db31859e11cbaa2961dd176732
Zargham Ahmad <zargham.ahmad12@gmail.com>
2023-12-12 14:57:54 +0100

    updated test

--- MFAssignR/tests/testthat/test-HistNoise.R
+++ MFAssignR/tests/testthat/test-HistNoise.R
@@ -1,6 +1,9 @@
 
 test_that("HistNoise works", {
-   load("../../data/Raw_Neg_ML.rda")
+   load(system.file("data", "Raw_Neg_ML.rda",
+    package = "MFAssignR",
+    mustWork = TRUE
+  ))
    actual <- HistNoise(Raw_Neg_ML)
    expect_equal(actual$Noise, 317.3483, tolerance = 1e-4)
  })
