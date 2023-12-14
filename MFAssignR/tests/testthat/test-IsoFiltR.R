f1b216d6eb211a9639813b54f9b13578cd3d04e7
hechth <helge.hecht@recetox.muni.cz>
2023-12-14 12:12:57 +0100

    updated tests with new testdata

--- MFAssignR/tests/testthat/test-IsoFiltR.R
+++ MFAssignR/tests/testthat/test-IsoFiltR.R
@@ -9,4 +9,12 @@ test_that("Isotope filtering works", {
   expect_equal(actual, expected)
 })
 
-
+patrick::with_parameters_test_that("IsoFiltR works",
+  {
+    raw <- read.csv(file.path("test-data", paste0("QC1_1_", toupper(mode), "_500.csv")))
+    expected <- readRDS(file.path("test-data", paste0(mode, "_iso.rds")))
+    actual <- IsoFiltR(raw)
+    expect_equal(actual, expected)
+  },
+  mode = c("pos", "neg")
+)
--- MFAssignR/tests/testthat/test-MFAssignCHO.R
+++ MFAssignR/tests/testthat/test-MFAssignCHO.R
@@ -1,18 +1,26 @@
-patrick::with_parameters_test_that("MFAssignCHO works", {
-  load(system.file("data", "Raw_Neg_ML.rda",
-                   package = "MFAssignR",
-                   mustWork = TRUE
-  ))
-  data <- IsoFiltR(Raw_Neg_ML)
-  actual <- MFAssignCHO(
-    peaks = data$Mono,
-    isopeaks = data$Iso,
-    ionMode = mode
-  )
-  expected <- readRDS(paste0("test-data/mfassignCHO_testData-", mode, ".rda"))
-  expect_equal(actual$Ambig, expected$Ambig)
-  expect_equal(actual$Unambig, expected$Unambig)
-  expect_equal(actual$None, expected$None)
-},
-mode = c("neg", "pos")
+load_expected <- function(mode) {
+  unambig <- readRDS(file.path("test-data", paste0(mode, "_cho_unambig.rds")))
+  ambig <- readRDS(file.path("test-data", paste0(mode, "_cho_ambig.rds")))
+  none <- readRDS(file.path("test-data", paste0(mode, "_cho_none.rds")))
+  return(list(Unambig = unambig, Ambig = ambig, None = none))
+}
+
+patrick::with_parameters_test_that("MFAssignCHO works",
+  {
+    peaks <- readRDS(file.path("test-data", paste0(mode, "_iso.rds")))
+    actual <- MFAssignCHO(
+      peaks = peaks$Mono,
+      isopeaks = peaks$Iso,
+      ionMode = mode
+    )
+
+    expected <- load_expected(mode)
+    # keys <- c("Ambig", "Unambig", "None")
+    # expect_equal(actual[keys], expected)
+
+    expect_equal(actual$Ambig, expected$Ambig)
+    expect_equal(actual$Unambig, expected$Unambig)
+    expect_equal(actual$None, expected$None)
+  },
+  mode = c("pos", "neg")
 )
