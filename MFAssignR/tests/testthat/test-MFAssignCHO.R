f5b88a1e8562e1785ea09c7822628da7b182028c
hechth <helge.hecht@recetox.muni.cz>
2023-12-14 09:51:52 +0100

    switched to only comparing the dataframes

--- MFAssignR/tests/testthat/test-MFAssignCHO.R
+++ MFAssignR/tests/testthat/test-MFAssignCHO.R
@@ -1,19 +1,18 @@
 patrick::with_parameters_test_that("MFAssignCHO works", {
-#  load("../../data/Raw_Neg_ML.rda")
-#   data <- system.file('data', 'Raw_Neg_ML.rda', package = "MFAssignR")
   load(system.file("data", "Raw_Neg_ML.rda",
                    package = "MFAssignR",
                    mustWork = TRUE
   ))
-  data <- MFAssignR::IsoFiltR(Raw_Neg_ML)
-  actual <- MFAssignR::MFAssignCHO(
+  data <- IsoFiltR(Raw_Neg_ML)
+  actual <- MFAssignCHO(
     peaks = data$Mono,
     isopeaks = data$Iso,
     ionMode = mode
   )
   expected <- readRDS(paste0("test-data/mfassignCHO_testData-", mode, ".rda"))
-  expect_equal(actual, expected)
+  expect_equal(actual$Ambig, expected$Ambig)
+  expect_equal(actual$Unambig, expected$Unambig)
+  expect_equal(actual$None, expected$None)
 },
 mode = c("neg", "pos")
 )
-
