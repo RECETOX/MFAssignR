1994ee298557b0363cc3de21d7528307e5126225
KristinaGomoryova <423436@mail.muni.cz>
2023-12-18 15:52:59 +0100

    paths to data loading adjusted

--- MFAssignR/tests/testthat/test-HistNoise.R
+++ MFAssignR/tests/testthat/test-HistNoise.R
@@ -1,8 +1,5 @@
 test_that("HistNoise works", {
-  load(system.file("data", "Raw_Neg_ML.rda",
-    package = "MFAssignR",
-    mustWork = TRUE
-  ))
+  load(file.path("test-data", "Raw_Neg_ML.rda"))
   actual <- HistNoise(Raw_Neg_ML)
   expect_equal(actual$Noise, 317.3483, tolerance = 1e-4)
 })
--- MFAssignR/tests/testthat/test-KMDNoise.R
+++ MFAssignR/tests/testthat/test-KMDNoise.R
@@ -1,8 +1,5 @@
 test_that("KMDNoise works", {
-  load(system.file("data", "Raw_Neg_ML.rda",
-    package = "MFAssignR",
-    mustWork = TRUE
-  ))
+  load(file.path("test-data", "Raw_Neg_ML.rda"))
   names(Raw_Neg_ML) <- c("mass", "intensity")
   actual <- KMDNoise(Raw_Neg_ML)
   expect_equal(actual$Noise, 346.0706, tolerance = 1e-4)
@@ -10,10 +7,7 @@ test_that("KMDNoise works", {
 
 
 test_that("SNplot works", {
-  load(system.file("data", "Raw_Neg_ML.rda",
-    package = "MFAssignR",
-    mustWork = TRUE
-  ))
+  load(file.path("test-data", "Raw_Neg_ML.rda"))
   Raw_Neg_ML <- dplyr::relocate(Raw_Neg_ML, intensity) |> dplyr::rename(mass = m.z)
   SNplot(Raw_Neg_ML, 1, 300, 100, 1000)
 })
