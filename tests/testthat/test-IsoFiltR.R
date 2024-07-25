5265322a30677d78f6c2d3f5f29b568720d4e206
Helge Hecht <helge.hecht@recetox.muni.cz>
2024-07-25 10:20:05 +0200

    fixed tests after update

--- tests/testthat/test-IsoFiltR.R
+++ tests/testthat/test-IsoFiltR.R
@@ -1,8 +1,8 @@
 patrick::with_parameters_test_that("IsoFiltR works",
   {
     raw <- read.csv(file.path("test-data", paste0("QC1_1_", toupper(mode), "_500.csv")))
-    expected <- readRDS(file.path("test-data", paste0(mode, "_iso.rds")))
     actual <- IsoFiltR(raw)
+    expected <- readRDS(file.path("test-data", paste0(mode, "_iso.rds")))
     expect_equal(actual, expected)
   },
   mode = c("pos", "neg")
