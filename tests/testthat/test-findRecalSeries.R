4c23182d0b23473d4934b61ce42a84d1709ca0b3
KristinaGomoryova <gomoryova@sci.muni.cz>
2024-09-03 11:43:09 +0200

    tests for the compute_combinations and compute_subsets

--- tests/testthat/test-findRecalSeries.R
+++ tests/testthat/test-findRecalSeries.R
@@ -4,3 +4,17 @@ test_that("Compute coverage works", {
   expected <- 23.21534
   expect_equal(actual, expected, tolerance = 0.0005)
 })
+
+test_that("Compute combinations works", {
+  df <- data.frame(series = c(1:5))
+  expected <- readRDS("test-data/combinations_simple.rds")
+  actual <- compute_combinations(df, 3)
+  expect_equal(actual, expected)
+})
+
+test_that("Compute subsets from combinations", {
+  df <- head(readRDS(file.path("test-data", "pos_recalSeries.rds")), 5)
+  expected <- readRDS("test-data/combination_subsets.rds")
+  actual <- compute_subsets(df, 3)
+  expect_equal(actual, expected)
+})
