a0c72c841b578d607ef7083913111bb4248a6b2a
KristinaGomoryova <gomoryova@sci.muni.cz>
2024-09-04 12:00:10 +0200

    test for computing final scores

--- tests/testthat/test-findRecalSeries.R
+++ tests/testthat/test-findRecalSeries.R
@@ -6,13 +6,6 @@ test_that("Filtering input dataframe works", {
   expect_equal(actual, expected)
 })
 
-test_that("Compute coverage works", {
-  pos_recalList <- readRDS(file.path("test-data", "pos_recalSeries.rds"))
-  actual <- compute_coverage(pos_recalList, 499.326, 111.044)
-  expected <- 23.21534
-  expect_equal(actual, expected, tolerance = 0.0005)
-})
-
 test_that("Compute combinations works", {
   df <- data.frame(series = c(1:5))
   expected <- readRDS("test-data/combinations_simple.rds")
@@ -27,13 +20,27 @@ test_that("Compute subsets from combinations works", {
   expect_equal(actual, expected)
 })
 
-test_that("Filtering of the subsets work", {
+test_that("Compute coverage works", {
+  pos_recalList <- readRDS(file.path("test-data", "pos_recalSeries.rds"))
+  actual <- compute_coverage(pos_recalList, 499.326, 111.044)
+  expected <- 23.21534
+  expect_equal(actual, expected, tolerance = 0.0005)
+})
+
+test_that("Filtering of the subsets based on coverage works", {
   df <- readRDS("test-data/combination_subsets.rds")
   expected <- readRDS("test-data/filtered_subsets.rds")
   actual <- filter_subsets_based_on_coverage(df, 80, 206, 117)
   expect_equal(actual, expected)
 }
 )
+
+test_that("Computing final scores work", {
+  df <- head(readRDS("test-data/scores_df.rds"), 6)
+  expected <- readRDS("test-data/final_scores.rds")
+  actual <- compute_final_score(df)
+  expect_equal(actual, expected)
+})
 
 patrick::with_parameters_test_that("Selection of the final series works",
   {
